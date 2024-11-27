c9642bbe3c827c48fb7b9c7d138720084f2e4ffd
Pedro Henrique <phrsilva.adv@gmail.com>
2024-11-27 14:06:49 -0300

    inicío da aula react

---
+++ Stage 9 - ReactJS/introReactJS/src/pages/App/index.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import './styles.css'
+export function App () {
+  
+  return (
+    <Fragment>
+
+      <h1>Hello React</h1>
+      <p>Por Pedro Henrique</p>
+    </Fragment>
+  )
+}
