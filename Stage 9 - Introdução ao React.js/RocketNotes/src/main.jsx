2de094914227beccbfc6d2936a856287cfad90f5
megafisto <phrsilva.adv@gmail.com>
2024-11-30 22:43:38 -0300

    reiniciando aula

---
+++ Stage 9 - Introdução ao React.js/RocketNotes/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Details } from './pages/Details.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Details />
+  </StrictMode>
+)
---
+++ Stage 9 - Introdução ao React.js/RocketNotes/src/pages/Details.jsx
@@ -0,0 +1,8 @@
+export function Details() {
+  return (
+    <>
+      <h1>rocketnotes</h1>
+      <p>um aplicativo de anotações para o dia-a-dia</p>
+    </>
+  )
+}
