65639691783317f02748f011464d4d4ea6ea109a
Pedro Henrique <phrsilva.adv@gmail.com>
2024-12-03 14:01:42 -0300

    continuação da aula

--- Stage 9 - Introdução ao React.js/RocketNotes/src/pages/details/index.jsx
+++ Stage 9 - Introdução ao React.js/RocketNotes/src/pages/details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./styles"
+import { Button } from "../../components/button"
 
 export function Details() {
   return (
     <Container>
       <h1>rocketnotes</h1>
       <p>um aplicativo de anotações para o dia-a-dia</p>
+      <Button title="Cadastrar" loading></Button>
+
+
     </Container>
-  )
+  ); 
 }
--- Stage 9 - Introdução ao React.js/RocketNotes/src/styles/global.js
+++ Stage 9 - Introdução ao React.js/RocketNotes/src/styles/global.js
@@ -5,6 +5,8 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Roboto Slab', serif;
+        -webkit-font-smoothing: antialiased;
     }
 
     :root {
