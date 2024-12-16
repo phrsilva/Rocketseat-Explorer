cef2fb14bc5518f78781aa703c40b072e55dc565
Pedro Henrique <phrsilva.adv@gmail.com>
2024-12-16 08:34:29 -0300

    correções

--- Stage 9 - Introdução ao React.js/RocketNotes/src/pages/home/index.jsx
+++ Stage 9 - Introdução ao React.js/RocketNotes/src/pages/home/index.jsx
@@ -12,7 +12,7 @@ export function Home() {
     return (
         <Container>
             <Brand>
-                <h1>RocketNotes</h1>
+                <h1>RocketNuotes</h1>
             </Brand>
 
             <Header>
