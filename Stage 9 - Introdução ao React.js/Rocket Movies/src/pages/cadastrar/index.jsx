4eeb1ae8218b1ab65d1dd1434a2d498edc984496
Pedro Henrique <phrsilva.adv@gmail.com>
2024-12-21 20:29:07 -0300

    conclusão das páginas de autenticação

--- Stage 9 - Introdução ao React.js/Rocket Movies/src/pages/cadastrar/index.jsx
+++ Stage 9 - Introdução ao React.js/Rocket Movies/src/pages/cadastrar/index.jsx
@@ -1,5 +1,26 @@
+import { Container, Form, PlanoDeFundo } from "./styles";
+import { Input } from "../../components/input";
+import { Button } from "../../components/button";
+import { Link } from "react-router-dom";
+import { BotaoDeTexto } from "../../components/textButton";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
+
 export const Cadastrar = () => {
     return (
-        <h1>Cadastrar</h1>
+        <Container>
+            <Form>
+                <h1>RocketMovies</h1>
+                <p>Aplicação para acompanhar filmes e séries</p>
+                <Input icon={FiUser} placeholder="Nome"></Input>
+                
+                <Input icon={FiMail} placeholder="E-mail" />
+                <Input type="password" icon={FiLock} placeholder="Senha" />
+                <Input type="password" icon={FiLock} placeholder="Confirmar Senha"/>
+                <Button title="Criar conta" />
+               
+
+            </Form>
+            <PlanoDeFundo/>
+        </Container>
     );
 }
--- Stage 9 - Introdução ao React.js/Rocket Movies/src/pages/login/index.jsx
+++ Stage 9 - Introdução ao React.js/Rocket Movies/src/pages/login/index.jsx
@@ -1,5 +1,25 @@
+import { Container, Form, PlanoDeFundo } from "./styles";
+import { Input } from "../../components/input";
+import { Button } from "../../components/button";
+import { Link } from "react-router-dom";
+import { BotaoDeTexto } from "../../components/textButton";
+import { FiMail, FiLock } from "react-icons/fi";
+
 export const Login = () => {
     return (
-        <h1>Login</h1>
+        <Container>
+            <Form>
+                <h1>RocketMovies</h1>
+                <p>Aplicação para acompanhar filmes e séries</p>
+                <Input icon={FiMail} placeholder="E-mail" />
+                <Input type="password" icon={FiLock} placeholder="Senha" />
+                <Button title="Entrar" />
+                <BotaoDeTexto title="Criar conta" >
+                    <Link to="/cadastrar" />
+                </BotaoDeTexto>
+
+            </Form>
+            <PlanoDeFundo/>
+        </Container>
     );
 }
