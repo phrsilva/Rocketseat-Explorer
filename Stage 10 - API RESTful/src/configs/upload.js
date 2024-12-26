cb8a4dd7e59df014b7a96aab92e4d92fd7ef9ba1
megafisto <phrsilva.adv@gmail.com>
2024-12-25 23:05:39 -0300

    upload de imagens

--- Stage 10 - API RESTful/src/configs/upload.js
+++ Stage 10 - API RESTful/src/configs/upload.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const multer = require('multer');
+const crypto = require('crypto');
 
 const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp');
 const UPLOADS_FOLDER = path.resolve(__dirname, '..', '..', 'uploads');
+
 const MULTER = {
     storage: multer.diskStorage({
         destination: TMP_FOLDER,
         filename(request, file, callback) {
-            const fileHash = Date.now() + '-' + file.originalname;
-            const fileName = path.resolve(UPLOADS_FOLDER, fileHash);
+            const fileHash = crypto.randomBytes(10).toString('hex');
+            const fileName = `${fileHash}-${file.originalname}`;
             callback(null, fileName);
-        }
+        },
         
-    })
+    }),
     
 }
 
@@ -20,5 +22,6 @@ const MULTER = {
 module.exports = {
     TMP_FOLDER,
     UPLOADS_FOLDER: path.resolve(TMP_FOLDER, 'uploads'),
+    MULTER
 }
 
--- Stage 10 - API RESTful/src/routes/users.routes.js
+++ Stage 10 - API RESTful/src/routes/users.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require('express');
 const ensureAuth = require('../middleware/ensureAuth');
+const multer = require('multer');
+
+const uploadConfig = require('../configs/upload');
 
 const UsersController = require('../controllers/UsersController');
 const AppError = require('../utils/AppError');
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 
 const usersController = new UsersController();
@@ -12,6 +16,10 @@ const usersController = new UsersController();
 
 usersRoutes.post('/',  usersController.create);
 usersRoutes.put('/', ensureAuth, usersController.update);
+usersRoutes.patch('/avatar', ensureAuth, upload.single('avatar'), (request, response) => { 
+    console.log(request.file.filename);
+    response.json();
+});
 
 
 module.exports = usersRoutes;
