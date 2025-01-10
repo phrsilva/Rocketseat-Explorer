8b8628f70bb5695673296ffa455b15277931808b
Pedro Henrique <phrsilva.adv@gmail.com>
2025-01-10 13:08:59 -0300

    merge

--- Stage 10 - API RESTful/backend/src/controllers/TagsController.js
+++ Stage 10 - API RESTful/backend/src/controllers/TagsController.js
@@ -5,7 +5,7 @@ class TagsController {
     async index(request, response) {
         const  user_id = request.user.id
         
-        const tags = await knex('tags').where({ user_id })
+        const tags = await knex('tags').where({ user_id }).groupBy('name').orderBy('name');
 
         return response.json(tags);
     }
