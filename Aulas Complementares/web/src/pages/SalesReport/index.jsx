cdcd3f8287a8fba3d19bd29246f4e07135696029
megafisto <phrsilva.adv@gmail.com>
2025-01-26 19:29:44 -0300

    uso de cookie na requisição

--- Aulas Complementares/web/src/pages/SalesReport/index.jsx
+++ Aulas Complementares/web/src/pages/SalesReport/index.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from "../../services/api";
 
 export function SalesReport() {
+  useEffect(() => {
+    api
+    .get('/sales', {withCredentials: true})
+    .then(response => console.log(response.data))
+  }, [])
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
