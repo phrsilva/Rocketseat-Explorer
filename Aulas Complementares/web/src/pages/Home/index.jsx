6ee3b03be6fdb1648605fb4cb3ad18e2810c5cf7
megafisto <phrsilva.adv@gmail.com>
2025-01-26 18:49:37 -0300

    exibição da home conforme perfil do usuário

--- Aulas Complementares/web/src/pages/Home/index.jsx
+++ Aulas Complementares/web/src/pages/Home/index.jsx
@@ -3,16 +3,30 @@ import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi';
 import { Container } from "./styles";
 import { Feature } from '../../components/Feature';
 import { Header } from '../../components/Header';
+import { useAuth } from '../../hooks/auth';
+
+
 
 export function Home() {
+  
+  const { user } = useAuth();
+  
   return (
     <Container>
       <Header />
 
       <main>
         <Feature title="Produto" icon={FiTag} to="/product" />
-        <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />
-        <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+
+        {
+          user.role === 'admin' || user.role === 'sale' &&
+          <>
+          {user.role === 'admin' && <Feature title="Fornecedores" icon={FiTruck} to="/suppliers" />}
+          <Feature title="Relatório de vendas" icon={FiShoppingCart} to="/sales-report" />
+          </>
+        }
+
+
       </main>
     </Container>
   )
