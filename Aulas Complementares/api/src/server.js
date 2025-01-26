ee340611342565eca862317105fd1212b3a65a9f
megafisto <phrsilva.adv@gmail.com>
2025-01-26 19:12:32 -0300

    token nos cookies

--- Aulas Complementares/api/src/server.js
+++ Aulas Complementares/api/src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors");
 
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const routes = require("./routes");
 
@@ -8,7 +9,11 @@ const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  credentials: true
+}));
 
 app.use(routes);
 
