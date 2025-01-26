bf38a215d7a119ef04185f10e32cb4206bca8482
megafisto <phrsilva.adv@gmail.com>
2025-01-26 19:45:54 -0300

    filtrando erro de autorização no login

--- Aulas Complementares/api/src/routes/users.routes.js
+++ Aulas Complementares/api/src/routes/users.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const UsersController = require("../controllers/UsersController");
+const UserValidatedController = require("../controllers/UserValidatedController");
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const userValidatedController = new UserValidatedController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated, userValidatedController.index);
 
 module.exports = usersRoutes;
--- Aulas Complementares/web/src/routes/index.jsx
+++ Aulas Complementares/web/src/routes/index.jsx
@@ -1,14 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
-
+import { api } from '../services/api';
 import { AdminRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
 import { CustomerRoutes } from './customer.routes';
 import { SaleRoutes } from './sale.routes';
+import { useEffect } from 'react';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api
+    .get('/users/validated').catch(error => {
+      if(error.response.status === 401){
+        
+        signOut()
+        }
+    })
+  }, [])
+
+  
 
   function AccessRoute(){
     switch(user.role){
